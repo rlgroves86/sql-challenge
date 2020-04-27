1d948694582c14685152b4651bcd33193ea02c5f
rlgroves86 <59593741+rlgroves86@users.noreply.github.com>
2020-04-26 21:57:16 -0700

    Add files via upload

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS data_departments;
+DROP TABLE IF EXISTS data_dept_emp;
+DROP TABLE IF EXISTS data_dept_manager;
+DROP TABLE IF EXISTS data_employees;
+DROP TABLE IF EXISTS data_salaries;
+DROP TABLE IF EXISTS data_titles;
+
+CREATE TABLE "data_departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "data_dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "data_employees" (
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "data_salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "data_titles" (
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+SELECT * FROM data_departments;
+SELECT * FROM data_dept_emp;
+SELECT * FROM data_dept_manager;
+SELECT * FROM data_employees;
+SELECT * FROM data_salaries;
+SELECT * FROM data_titles;
